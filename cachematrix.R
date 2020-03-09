29f736fc17bfe7a59f2ab72d9199869a085e6752
DJARBOL <danielarbolmix@gmail.com>
2020-03-09 00:40:12 -0500

    Edited R script with my version of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming - Assingment 2
+## Daniel Arboleda M.
 
-## Write a short comment describing this function
+## According to the assignment guidelines, this functions are
+## meant to calculate the inverse of a square invertible matrix
+## and store it in the cache so that R identifies if it has been
+## calculated before and takes it from the memory.
+
+## This function creates a matrix that cache's its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  MatrixInv <- NULL
+  SetMat <- function(y) {
+    x <<- y
+    MatrixInv <<- NULL
+  }
+  GetMat <- function() x
+  SetMatInv <- function(solve) MatrixInv <<- solve
+  GetMatInv <- function() MatrixInv
+  list(SetMat = SetMat, GetMat = GetMat,
+       SetMatInv = SetMatInv,
+       GetMatInv = GetMatInv)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix with the
+## above function, unless it is already calculated, in which case,
+## it takes the inverse from the cache and skips the computation.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  MatrixInv <- x$GetMatInv()
+  if(!is.null(MatrixInv)) {
+    message("getting cached inverted matrix")
+    return(MatrixInv)
+  }
+  MatData <- x$GetMat()
+  MatrixInv <- solve(MatData, ...)
+  x$SetMatInv(MatrixInv)
+  MatrixInv
 }
+
+##Time to test the functions
+MT1 <- matrix(1:4, 2, 2)
+t1 <- makeCacheMatrix(MT1)
+t1$GetMat()
+t1$GetMatInv()
+
+cacheSolve(t1)
+
+MT2 <- matrix(c(5, 8, 4, 7, 3, 6, 6, 2, 1), 3, 3)
+t2 <- makeCacheMatrix(MT2)
+t2$GetMat()
+t2$GetMatInv()
+cacheSolve(t2)
